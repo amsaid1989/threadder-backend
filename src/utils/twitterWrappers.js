235c0a94458bf9b6c2fdda6eb5da2b653e160d47
Abdelrahman <said.abdelrahman89@gmail.com>
2021-08-06 02:06:20 +0100

    Added comments to the twitterWrappers functions and checked the .yarn directory into git to ensure heroku builds work

--- src/utils/twitterWrappers.js
+++ src/utils/twitterWrappers.js
@@ -22,6 +22,12 @@ const oauth = OAuth({
 });
 
 function addTwitterAuthorizationHeader(request, token = undefined) {
+    /*
+     * Adds an authorization header to an HTTP request. If a token is
+     * provided, then it is included in the header. Otherwise, the
+     * header will only use the application public and secret keys.
+     */
+
     const oauth_data = token
         ? oauth.authorize(request, token)
         : oauth.authorize(request);
@@ -33,15 +39,23 @@ function addTwitterAuthorizationHeader(request, token = undefined) {
 }
 
 function sendTwitterRequest(requestObj) {
+    /*
+     * Utility function that builds a request from the object passed
+     * to it, then adds the required authorization headers before
+     * returning the result.
+     */
+
     const requestForAuth = {
         url: requestObj.url,
         method: requestObj.method,
     };
 
+    // Add data to the axios request if a data object is provided
     if (requestObj.data && Object.entries(requestObj.data).length > 0) {
         requestForAuth.data = requestObj.data;
     }
 
+    // Add params to the axios request if a params object is provided
     if (requestObj.params && Object.entries(requestObj.params).length > 0) {
         requestForAuth.params = requestObj.params;
     }
@@ -50,6 +64,14 @@ function sendTwitterRequest(requestObj) {
 }
 
 export function getRequestToken() {
+    /*
+     * Initiates the first step of the 3-legged process of logging in
+     * with Twitter.
+     *
+     * It makes a call to the 'request_token' endpoint and returns the
+     * result.
+     */
+
     return sendTwitterRequest({
         url: "https://api.twitter.com/oauth/request_token",
         method: "post",
@@ -58,6 +80,15 @@ export function getRequestToken() {
 }
 
 export function getAccessToken(twitterResponseObj) {
+    /*
+     * Initiates the last step of the 3-legged process of logging in
+     * with Twitter.
+     *
+     * It makes a request to the 'access_token' endpoint in order to
+     * get the user's tokens for the app after being granted access
+     * to the user's account.
+     */
+
     return sendTwitterRequest({
         url: "https://api.twitter.com/oauth/access_token",
         method: "post",
@@ -66,6 +97,11 @@ export function getAccessToken(twitterResponseObj) {
 }
 
 export function getUserData(id) {
+    /*
+     * Sends a request to Twitter's '/users/show' endpoint to get
+     * the user's data given a valid id.
+     */
+
     const url = queryString.stringifyUrl({
         url: "https://api.twitter.com/1.1/users/show.json",
         query: {
@@ -77,6 +113,14 @@ export function getUserData(id) {
 }
 
 function postTweet(text, token, previousTweetID = undefined) {
+    /*
+     * Makes a request to Twitter's 'statuses/update' endpoint to
+     * publish a tweet on behalf of a user.
+     *
+     * If previousTweetID is provided, then the new tweet is added
+     * as a reply to the previous tweet.
+     */
+
     const url = queryString.stringifyUrl({
         url: "https://api.twitter.com/1.1/statuses/update.json",
         query: {
@@ -93,6 +137,12 @@ function postTweet(text, token, previousTweetID = undefined) {
 }
 
 export async function publishThread(thread, token) {
+    /*
+     * Uses the postTweet() function to post a series of tweets
+     * as a thread, where each tweet is posted as a reply to the
+     * previous one.
+     */
+
     let lastID;
 
     for (const tweet of thread) {
