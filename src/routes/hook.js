2fa9b36d4fb092c27ec56cd3dae252a0edbf5bd0
Abdelrahman Said <said.abdelrahman89@gmail.com>
2022-02-27 23:44:15 +0000

    Switched to using execSync

--- src/routes/hook.js
+++ src/routes/hook.js
@@ -1,23 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
-import { exec } from "child_process";
+import { execSync } from "child_process";
 import logger from "../utils/logger.js";
-
-function executeCommand(cmd) {
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                reject(error.message);
-            }
-
-            if (stderr) {
-                reject(stderr);
-            }
-
-            resolve(stdout);
-        });
-    });
-}
 
 dotenv.config();
 
@@ -50,34 +34,24 @@ router.post("/hook", (req, res) => {
                 ? "git pull --all&&pm2 restart --update-env threadder"
                 : "git pull --all";
 
-        executeCommand(cmd)
-            .then((result) => {
-                logger.info(result);
+        const output = execSync(cmd);
+        console.log(output);
 
-                res.status(200).send("Command succeeded");
-            })
-            .catch((err) => {
-                logger.error(err);
-
-                res.status(500).send("Command failed");
-            });
+        res.status(200).send("Completed");
     } else if (name === "threadder") {
-        const cmd =
-            process.env.NODE_ENV === "production"
-                ? "git submodule update --remote --recursive&&pm2 restart --update-env threadder"
-                : "git submodule update --remote --recursive";
-
-        executeCommand(cmd)
-            .then((result) => {
-                logger.info(result);
-
-                res.status(200).send("Command succeeded");
-            })
-            .catch((err) => {
-                logger.error(err);
-
-                res.status(500).send("Command failed");
-            });
+        // const cmd =
+        //     process.env.NODE_ENV === "production"
+        //         ? "git submodule update --remote --recursive&&pm2 restart --update-env threadder"
+        //         : "git submodule update --remote --recursive";
+        // executeCommand(cmd)
+        //     .then((result) => {
+        //         logger.info(result);
+        //         res.status(200).send("Command succeeded");
+        //     })
+        //     .catch((err) => {
+        //         logger.error(err);
+        //         res.status(500).send("Command failed");
+        //     });
     }
 });
 
