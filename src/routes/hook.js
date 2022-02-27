90fb014f55e558fc758c64488b70ea7e57543523
Abdelrahman Said <said.abdelrahman89@gmail.com>
2022-02-27 21:41:24 +0000

    Fixed a typo in import dotenv in the hook.js route file

--- src/routes/hook.js
+++ src/routes/hook.js
@@ -1,9 +1,9 @@
-import dotenv from dotenv
+import dotenv from "dotenv";
 import express from "express";
-import {exec} from "child_process";
+import { exec } from "child_process";
 import logger from "../utils/logger.js";
 
-dotenv.config()
+dotenv.config();
 
 const router = express.Router();
 
@@ -13,47 +13,47 @@ router.post("/hook", (req, res) => {
     const { name } = req.body.repository;
 
     if (name === "threadder-backend") {
-        const cmd = process.env.NODE_ENV === 'production' ? "git pull --all&&pm2 restart --update-env threadder" : "git pull --all"
-        exec(
-            cmd,
-            (error, stdout, stderr) => {
-                if (error) {
-                    logger.error(error.message);
-                    res.sendStatus(500);
-                    return;
-                }
-
-                if (stderr) {
-                    logger.error(stderr);
-                    res.sendStatus(500);
-                    return;
-                }
-
-                logger.info(stdout);
-                res.sendStatus(200);
+        const cmd =
+            process.env.NODE_ENV === "production"
+                ? "git pull --all&&pm2 restart --update-env threadder"
+                : "git pull --all";
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                logger.error(error.message);
+                res.sendStatus(500);
+                return;
             }
-        );
+
+            if (stderr) {
+                logger.error(stderr);
+                res.sendStatus(500);
+                return;
+            }
+
+            logger.info(stdout);
+            res.sendStatus(200);
+        });
     } else if (name === "threadder") {
-        const cmd = process.env.NODE_ENV === 'production' ? "git submodule update --remote --recursive&&pm2 restart --update-env threadder" : "git submodule update --remote --recursive"
-        exec(
-            cmd,
-            (error, stdout, stderr) => {
-                if (error) {
-                    logger.error(error.message);
-                    res.sendStatus(500);
-                    return;
-                }
-
-                if (stderr) {
-                    logger.error(stderr);
-                    res.sendStatus(500);
-                    return;
-                }
-
-                logger.info(stdout);
-                res.sendStatus(200);
+        const cmd =
+            process.env.NODE_ENV === "production"
+                ? "git submodule update --remote --recursive&&pm2 restart --update-env threadder"
+                : "git submodule update --remote --recursive";
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                logger.error(error.message);
+                res.sendStatus(500);
+                return;
             }
-        );
+
+            if (stderr) {
+                logger.error(stderr);
+                res.sendStatus(500);
+                return;
+            }
+
+            logger.info(stdout);
+            res.sendStatus(200);
+        });
     }
 });
 
